92668cb0052e7e4127102c2de272d2b60369abc0
Simon Lissack <lissack.simon@gmail.com>
2019-08-04 20:24:20 +0100

    First pass at adding dependency injection into app

--- AzureResourceMonitoring.Application/Program.cs
+++ AzureResourceMonitoring.Application/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using AzureResourceMonitoring.Infrastructure.Azure.Authentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AzureResourceMonitoring.Application
 {
@@ -10,21 +11,44 @@ namespace AzureResourceMonitoring.Application
     {
         static async Task Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
+            var configuration = SetupConfiguration();
+            var serviceCollection = SetupServiceCollection();
+
+            ConfigureAzureInfrastructure(configuration, serviceCollection);
+
+            using (var serviceProvider = serviceCollection.BuildServiceProvider())
+            {
+                var servicePrincipalProvider = serviceProvider.GetService<IServicePrincipalProvider>();
+                var credentials = await servicePrincipalProvider.GetCredentialsFromKeyVault();
+
+                Console.WriteLine($"Client: {credentials.ClientId}");
+                Console.WriteLine($"Tenant: {credentials.TenantId}");
+            }
+        }
+
+        static IConfiguration SetupConfiguration()
+        {
+            return new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true)
                 .AddUserSecrets<Program>(optional: true)
                 .Build();
+        }
 
+        static IServiceCollection SetupServiceCollection()
+        {
+            return new ServiceCollection();
+        }
+
+        static void ConfigureAzureInfrastructure(IConfiguration configuration, IServiceCollection services)
+        {
             var keyVaultConfig = new KeyVaultConfiguration();
             configuration.Bind(ConfigurationKeys.KeyVaultPrefix, keyVaultConfig);
 
-            var servicePrincipalProvider = new ServicePrincipalProvider(keyVaultConfig);
+            services
+                .AddSingleton(keyVaultConfig)
+                .AddTransient<IServicePrincipalProvider, ServicePrincipalProvider>();
 
-            var credentials = await servicePrincipalProvider.GetCredentialsFromKeyVault();
-
-            Console.WriteLine($"Client: {credentials.ClientId}");
-            Console.WriteLine($"Tenant: {credentials.TenantId}");
         }
 
         static class ConfigurationKeys
